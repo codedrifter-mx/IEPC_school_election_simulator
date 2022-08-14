de3af8f96e7bb7ed21cdc9284bb4945c85def979
Alfredo Flores Garcia <42259670+Alfredo-Flores@users.noreply.github.com>
2022-08-13 19:05:07 -0500

    Revert "First Commit backup"

--- database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -24,11 +23,6 @@ return new class extends Migration
             $table->timestamp('expires_at')->nullable();
             $table->timestamps();
         });
-
-        $table = "personal_access_tokens";
-        $comment = "Tokens";
-
-        DB::statement("ALTER TABLE " . $table . " COMMENT = '" . $comment . "'");
     }
 
     /**
